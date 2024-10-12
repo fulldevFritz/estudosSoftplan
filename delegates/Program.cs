fce6714166c67c80d3bb3887fd3b744ba76e256f
fritzlumen <fritzlumen@hotmail.com>
2024-10-12 09:48:29 -0300

    action e func

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Action clear = Console.Clear;
+
+Action<string?> exibir = Console.WriteLine;
+
+Func<int, int, int> somar = (x, y) => x + y;
+
+Func<int, int, int> subtrair = (x, y) => x - y;
+
+Func<int, int, int> multiplicar = (x, y) => x * y;
+
+Func< int, int, int> dividir = (x, y) => x / y;
+
+clear();
+
+exibir(somar(1, 1).ToString());
+exibir(subtrair(2, 1).ToString());
+exibir(multiplicar(1, 1).ToString());
+exibir(dividir(1, 1).ToString());
