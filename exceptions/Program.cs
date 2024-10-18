c8a24aac1aa5922f4e9e36a3bfd25bf65900fca7
fritzlumen <fritzlumen@hotmail.com>
2024-10-18 09:49:24 -0300

    Estudo de excecoes

---
+++ exceptions/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.Clear();
+
+var arr = new int [3];
+
+try
+{
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     //ArgumentNullException
+    //     //IndexOutOfRangeException
+    //     Console.WriteLine(arr[i]);
+    // }    
+    //Cadastrar(string.Empty);
+    //CadastrarGenerico(string.Empty);
+    CadastrarMinhaExcecao(string.Empty);
+}
+catch (IndexOutOfRangeException ex) {
+    Console.WriteLine("Não encontrei o indice na lista");
+    Console.WriteLine(ex.Message);
+}
+catch(ArgumentNullException ex){
+    Console.WriteLine("Falha ao cadastrar o texto");
+    Console.WriteLine(ex.Message);
+}
+catch(MyException ex) {
+    Console.WriteLine("Caiu na minha Exceção");
+    Console.WriteLine(ex.QuandoAconteceu);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo deu errado..");
+    Console.WriteLine(ex.Message);
+}
+
+static void Cadastrar(string texto){
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new ArgumentNullException("Não pode ser nulo ou vazio");
+    }
+}
+
+static void CadastrarGenerico(string texto){
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new Exception("Não pode ser nulo ou vazio");
+    }
+}
+
+static void CadastrarMinhaExcecao(string texto){
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new MyException(DateTime.Now);
+    }
+}
+
+public class MyException: Exception{
+    public MyException(DateTime quandoAconteceu)
+    {
+        QuandoAconteceu = quandoAconteceu;
+    }
+    public DateTime QuandoAconteceu { get; set; }
+}
+
+
